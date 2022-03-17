c84ab2185d90d0ed39783d519ea536a7785f1c3a
Paul Breuil <tykapl.breuil@gmail.com>
2022-03-17 14:32:01 +0100

    First prototype for #72 fix

---
+++ lifecompanion/lc-app/src/main/java/org/lifecompanion/model/impl/categorizedelement/useaction/available/MinimizeStageAction.java
@@ -0,0 +1,79 @@
+/*
+ * LifeCompanion AAC and its sub projects
+ *
+ * Copyright (C) 2014 to 2019 Mathieu THEBAUD
+ * Copyright (C) 2020 to 2021 CMRRF KERPAPE (Lorient, France)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.lifecompanion.model.impl.categorizedelement.useaction.available;
+
+import java.util.Map;
+
+import org.lifecompanion.model.api.categorizedelement.useaction.UseActionEvent;
+import org.lifecompanion.model.api.categorizedelement.useaction.UseActionTriggerComponentI;
+import org.lifecompanion.model.api.usevariable.UseVariableI;
+import org.lifecompanion.model.impl.categorizedelement.useaction.SimpleUseActionImpl;
+import org.lifecompanion.model.api.categorizedelement.useaction.DefaultUseActionSubCategories;
+import org.lifecompanion.controller.lifecycle.AppModeController;
+import javafx.stage.Stage;
+import org.lifecompanion.util.javafx.FXThreadUtils;
+import org.lifecompanion.model.impl.exception.LCException;
+import org.lifecompanion.model.api.io.IOContextI;
+import org.lifecompanion.framework.commons.fx.io.XMLObjectSerializer;
+import org.jdom2.Element;
+
+/**
+ * Action to minimize the configuration window.
+ * @author Paul BREUIL <tykapl.breuil@gmail.com>
+ */
+public class MinimizeStageAction extends SimpleUseActionImpl<UseActionTriggerComponentI> {
+
+    public MinimizeStageAction() {
+        super(UseActionTriggerComponentI.class);
+        this.order = 0;
+        this.category = DefaultUseActionSubCategories.FRAME;
+        this.nameID = "action.minimize.stage.name";
+        this.staticDescriptionID = "action.minimize.stage.description";
+        this.configIconPath = "configuration/icon_change_stage_ratio.png";
+        this.parameterizableAction = false;
+        this.variableDescriptionProperty().set(getStaticDescription());
+    }
+
+    // Class part : "Execute"
+    //========================================================================
+    @Override
+    public void execute(final UseActionEvent eventP, final Map<String, UseVariableI<?>> variables) {
+        FXThreadUtils.runOnFXThread(() -> {
+            final Stage stage = AppModeController.INSTANCE.getUseModeContext().getStage();
+            stage.setIconified(true);
+        });
+    }
+    //========================================================================
+
+    // Class part : "XML"
+    //========================================================================
+    @Override
+    public Element serialize(final IOContextI contextP) {
+        Element elem = super.serialize(contextP);
+        XMLObjectSerializer.serializeInto(ChangeStageSizeAction.class, this, elem);
+        return elem;
+    }
+
+    @Override
+    public void deserialize(final Element nodeP, final IOContextI contextP) throws LCException {
+        super.deserialize(nodeP, contextP);
+        XMLObjectSerializer.deserializeInto(ChangeStageSizeAction.class, this, nodeP);
+    }
+    //========================================================================
+}
