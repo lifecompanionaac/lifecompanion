79c35561778b6bb13b213c1daff7d1b5db2ff0f8
lisahry <117349146+lisahry@users.noreply.github.com>
2024-11-15 16:56:08 +0100

    Fix CurSta error

--- lifecompanion-plugins/aac4all-wp2-plugin/src/main/java/org/lifecompanion/plugin/aac4all/wp2/controller/AAC4AllWp2Controller.java
+++ lifecompanion-plugins/aac4all-wp2-plugin/src/main/java/org/lifecompanion/plugin/aac4all/wp2/controller/AAC4AllWp2Controller.java
@@ -70,6 +70,7 @@ public enum AAC4AllWp2Controller implements ModeListenerI {
 
         WritingStateController.INSTANCE.textBeforeCaretProperty().addListener((obs, ov, nv) -> {
             //HashSet<Character> acceptedCharact = new HashSet<>(curStaCharacters.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()));
+           if(this.configuration!= null){
             this.configuration.getAllComponent()
                     .values()
                     .stream()
@@ -78,7 +79,7 @@ public enum AAC4AllWp2Controller implements ModeListenerI {
                     .map(c -> c.getActionManager().getFirstActionOfType(
                             UseActionEvent.OVER, CurStaUseAction.class))
                     .filter(Objects::nonNull)
-                    .forEach(CurStaUseAction::cleanLastText);
+                    .forEach(CurStaUseAction::cleanLastText);}
             predict = transformResult(LCCharPredictor.INSTANCE.predict(WritingStateController.INSTANCE.textBeforeCaretProperty().get(), acceptedCharact.size(), acceptedCharact));
             curStaIndex = 0;
             updateCurSta();
